c1db4bf72da01ac2bf327d21e9468114d1f5d167
Зеленцов Тимофій Максимович <timejzelencov7@gmail.com>
2025-09-29 11:08:56 +0300

    Update mean_var_std.py
    Initial solution for mean-variance calculator

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,13 @@
 import numpy as np
 
 def calculate(list):
-
-
-
-
+    arr = np.array(list).reshape(3, 3)
+    mean_Val = [arr.mean(axis = 0).tolist(), arr.mean(axis = 1).tolist(),arr.mean().item()]
+    calc_Varience = [arr.var(axis = 0).tolist(), arr.var(axis = 1).tolist(),arr.var().item()]
+    calc_Min = [arr.min(axis = 0).tolist(), arr.min(axis = 1).tolist(),arr.min().item()]
+    calc_Max = [arr.max(axis = 0).tolist(), arr.max(axis = 1).tolist(),arr.max().item()]
+    calc_Std = [arr.std(axis = 0).tolist(), arr.std(axis = 1).tolist(),arr.std().item()]
+    calc_Sum = [arr.sum(axis = 0, dtype = 'int32').tolist(), arr.sum(axis = 1, dtype = 'int32').tolist(),arr.sum(dtype = 'int32').item()]
+    calculations = {'mean': mean_Val, 'variance': calc_Varience,'standard deviation': calc_Std,
+                    'min': calc_Min, 'max': calc_Max, 'sum': calc_Sum}
     return calculations
