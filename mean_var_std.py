1aff7a27a01d102b657e8942c251dae0aa13007e
Kyo-Fox22 <kayle.gamundoy@gmail.com>
2025-12-01 16:22:22 +0800

    calculate function solution

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,24 @@
 import numpy as np
 
 def calculate(list):
+    if len(list) < 9:
+        raise ValueError('List must contain nine numbers.')
 
+    x = np.array(list).reshape(3,3)
 
+    calculations = {}
 
+    func = lambda func, x, ax = None: func(x, axis = ax).tolist()
+    methods = {
+        'mean': np.mean,
+        'variance': np.var,
+        'standard deviation': np.std,
+        'max': np.max,
+        'min': np.min,
+        'sum': np.sum
+    }
 
+    for method in methods:
+        calculations[method] = [func(methods[method], x, ax) for ax in [0, 1, None]]
+        
     return calculations
